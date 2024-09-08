02a4ad1b60f3be94f267a14fe6e7654c108bcedb
smr24425 <smr24425@gmail.com>
2024-09-08 16:49:44 +0800

    fixed config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
-  assetPrefix: isProd ? "/smr24425.github.io/" : "",
-  basePath: isProd ? "/smr24425.github.io" : "",
+  assetPrefix: isProd ? "/https://smr24425.github.io//" : "",
+  basePath: isProd ? "/https://smr24425.github.io/" : "",
 };
 
 export default nextConfig;
